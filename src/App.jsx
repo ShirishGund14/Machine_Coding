4954f366d6738680101c3d96b6c29f47484ad120
Shirish Gund <shirish14g@gmail.com>
2024-07-04 22:08:32 +0530

    Star Rating Component Done !!!

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StarRating from './components/StarRating'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
-shirish
+        <StarRating numberofstars={10}/>
       </div>
-</>
-      )
+    </>
+  )
 }
 
-      export default App
+export default App
---
+++ src/components/StarRating.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { IoMdStar } from "react-icons/io";
+
+const StarRating = ({ numberofstars }) => {
+
+  const [selectstar,SetSelectedStar]=useState();
+  const [hoverval,SetHoverVal]=useState();
+
+  const stars = [];
+
+  for (let i = 0; i < numberofstars; i++) {
+    stars.push(<IoMdStar />);
+  }
+
+
+
+  // console.log(stars)
+  return (
+
+    <div className='bg-gray-700 h-screen w-full p-5 flex justify-center items-center'>
+      {
+        stars.map((star, index) => {
+          return (
+            <div className='w-1/2  h-40 bg-gray-900 p-3 flex items-center justify-center '>
+              <span className={`text-gray-300 text-4xl cursor-pointer ${ (index<=hoverval || (hoverval===0 && index<=selectstar)) ?'text-yellow-500': ''} `} 
+              key={index}  
+              onClick={()=>SetSelectedStar(index)}
+              onMouseEnter={()=>SetHoverVal(index)}
+              onMouseLeave={()=>SetHoverVal(0)}
+              >
+                {star}
+              </span>
+            </div>
+          )
+        })
+      }
+
+
+    </div>
+  )
+}
+
+export default StarRating
